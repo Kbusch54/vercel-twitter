9b6af74c2f5baafd9dac554cf1fc70c95605a12f
Kevin Busch <kevinbusch@comcast.net>
2023-07-24 15:49:46 -0400

    lessened twet memn

--- api/index.py
+++ api/index.py
@@ -111,6 +111,7 @@ listBtn ='/html/body/div[1]/div/div/div[2]/main/div/div/div/div[1]/div/div[1]/di
 nextBtn = '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[6]/div'
 logInBtn = '//*[@id="layers"]/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[2]/div/div[1]/div/div/div'
 tweetBtn ='//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a'
+tweetButton = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div/div[2]/div[4]'
 nameInput='/html/body/div[1]/div/div/div[1]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[2]/div[2]/label/div/div[2]/div/input'
 class Account:
     def __init__(self, account, username, description):
@@ -206,7 +207,6 @@ def logIn():
     # enter the xpath
     # driver.find_element.__getattribute__
     # fill the number or mail
-    print('on twitter log in page')
     driver.find_element(
         by='xpath', value=inputUser).send_keys(user)
     # find the element next button 
@@ -216,8 +216,6 @@ def logIn():
     driver.find_element(
         by='xpath',
         value=nextBtn).click()
-    print('clicked next button')
-
     # adjust the sleep time according to your internet speed
     time.sleep(2)
 
@@ -229,27 +227,20 @@ def logIn():
     # find the element login button
     # request using xpath
     # clicking on that element
-    print('about to click log in button and send password')
     driver.find_element(by='xpath',value=logInBtn).click()
     # adjust the sleep time according to your internet speed
     time.sleep(2)
-    print('sent password')
     driver.find_element(by='xpath',value=tweetBtn).click()
     time.sleep(2)
-    print('clicked log in button')
 def tweetThis(tweet):
-    print('tweeting this')
-    print(tweet)
     twitter_log_in()
     time.sleep(2)
     driver.get("https://twitter.com/compose/tweet")
     time.sleep(2)
     driver.find_element(by='xpath',value=textBox).send_keys(tweet)
     time.sleep(2)
-    tweetButton = '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div[2]/div[2]/div/div/div[2]/div[4]'
     driver.find_element(by='xpath',value=tweetButton).click()
     driver.quit()
-    return "Tweeted: "+tweet
 def followList(list):
     logIn()
     for user in list:
@@ -501,11 +492,7 @@ def add_list(usersToAdd):
 iterator = 0
 @app.route("/api/tweets/<path:tweetToSend>")
 def tweets(tweetToSend):
-    # Filter the name argument to letters only using regular expressions. URL arguments
-    # can contain arbitrary text, so we restrict to safe characters only.
-  
-    tweet = tweetThis(tweetToSend)
-    print(tweet)
+    tweetThis(tweetToSend)
     return f'Tweeted: {tweetToSend}'
 
 logging.getLogger('flask_cors').level = logging.DEBUG

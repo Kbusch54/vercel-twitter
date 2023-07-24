28bb5bcaf8970a775a5afc7c7c1121e9307bb392
Kevin Busch <kevinbusch@comcast.net>
2023-07-24 15:32:20 -0400

    driver updated ipti

--- api/index.py
+++ api/index.py
@@ -139,6 +139,8 @@ def load_chrome_driver():
     options.add_argument("--disable-gpu")
     options.add_argument("--headless")
     options.add_argument('--no-sandbox')  
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(service=service, options=options)
     driver.maximize_window()
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")

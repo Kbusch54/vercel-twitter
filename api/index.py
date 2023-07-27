f7dd02917ffc857e2839094ce3213a68d32ab258
Andy <andrewVB2012@protonmail.com>
2023-07-27 10:28:08 -0400

    driver chrome

--- api/index.py
+++ api/index.py
@@ -143,8 +143,8 @@ def load_chrome_driver():
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument("--window-size=1920,1080")
     options.add_argument('--disable-software-rasterizer')
-    # driver = webdriver.Chrome(service=service, options=options)
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    driver = webdriver.Chrome(service=service, options=options)
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     # driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     # driver.execute_cdp_cmd('Network.setUserAgentOverride', {
     #     "userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'})
